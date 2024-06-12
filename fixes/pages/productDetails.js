c93a1d157d009fb92f4c78a6b21b0bb1a92dcd4d
DQTejaswini <98795912+DQTejaswini@users.noreply.github.com>
2024-06-12 12:42:36 +0530

    Folder structure

---
+++ fixes/pages/productDetails.js
@@ -0,0 +1,11 @@
+import { fixButton } from "@amaze/plugins";
+
+/**
+ * @fix some-element
+ * @selector .some-element
+ * @summary Fixes some element to be a button
+ */
+
+export default (() => {
+
+})();
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import demoFix from "./fixes/demo-fix/index.js";
+import productDetails from "./fixes/pages/productDetails.js";
 
 export default {
     demoFix,
+    productDetails,
 }
